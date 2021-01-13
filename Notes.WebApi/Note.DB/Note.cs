101ebe57d44977e94e5ff5517aa0d2653e71d7d6
28cmm <joshuafang1998@gmail.com>
2021-01-12 20:12:32 -0800

    first commit

---
+++ Notes.WebApi/Note.DB/Note.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Notes.DB
+{
+    public class NoteArray
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Notes.WebApi/Notes.Core/INotesServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Notes.DB;
+
+namespace Notes.Core
+{
+    public interface INotesServices
+    {
+        NoteArray CreateNote(NoteArray note);
+        NoteArray GetNote(int id);
+        List<NoteArray> GetNotes();
+        void DeleteNote(int id);
+
+        void EditNote(NoteArray note);
+    }
+}
